55288c82ec6a7c815e691f14fe8d312ac5c60c7c
AsamiSatough <55976748+AsamiSatough@users.noreply.github.com>
2021-01-26 13:58:45 +0200

    Works on bee

--- Assets/Scripts/AirPatrol.cs
+++ Assets/Scripts/AirPatrol.cs
@@ -7,29 +7,35 @@ public class AirPatrol : MonoBehaviour
     public Transform point1;
     public Transform point2;
     public float speed = 2.0f;
+    public float waitTime = 3f;
+    bool canGo = true;
 
     private void Start()
     {
         gameObject.transform.position = new Vector3(point1.position.x, point1.position.y, point1.position.z);
-        StartCoroutine(PausedFly());
     }
 
     private void Update()
     {
-      
-    }
-
-    IEnumerator PausedFly()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, point1.position, speed * Time.deltaTime);
+        if (canGo)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point1.position, speed * Time.deltaTime);
+        }
 
         if (transform.position == point1.position)
         {
             Transform t = point1;
-            yield return new WaitForSeconds(2.0f);
-
             point1 = point2;
             point2 = t;
+
+            canGo = false;
+            StartCoroutine(Waiting());
         }
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        canGo = true;
     }
 }
